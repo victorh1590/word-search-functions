86c046f9c322502774b30f4ad4ea59abd28d16c4
Victor Hugo Faria <victorfaria1590@gmail.com>
2021-12-14 15:16:37 -0300

    Initialize project. Implements function to map downward diagonal to sorted dictionary.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.AccessControl;
+using System;
+using static System.Console;
+using System.Linq;
+using System.Collections.Generic;
+
+WriteLine("Hello, World!");
+
+SortedDictionary<int, string> DescendentDiagonal(string input) 
+{
+    List<string> lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+    int numColumns = lines.First().Length;
+    int numLines = lines.Count();
+
+    SortedDictionary<int, string> result = new();
+    for(int i = 0; i < numLines; i++) {
+      for (int j = 0; j < numColumns; j++) {
+        if (result.ContainsKey(j-i)) result[j-i] += lines[i][j]; 
+        else result.Add(j-i, lines[i][j].ToString());
+      }
+    }
+
+    return result;
+}
+
+string grid =
+            "jefblpepre\n" +
+            "camdcimgtc\n" +
+            "oivokprjsm\n" +
+            "pbwasqroua\n";
+            
+            // "rixilelhrs\n" +
+            // "wolcqlirpc\n" +
+            // "screeaumgr\n" +
+            // "alxhpburyi\n" +
+            // "jalaycalmp\n" +
+            // "clojurermt";
+
+
+var result = DescendentDiagonal(grid);
+
+foreach(var x in result) {
+  WriteLine($"{x.Key} => {x.Value}");
+}
